a2473a7845bfa02d0e629bbba3d956d90a286092
FMomb <fmomberg@usm.cl>
2023-10-24 20:00:07 -0300

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Grupo 15</h1> <h1>Integrantes:<h1> "
-                         "<h3>Maximiliano Martínez<h3>""<h3>Felipe momberg<h3>" "<h3>Valentina Muñoz<h3>""<h3>Mateo Sáez<h3>")
+                         "<h3>Maximiliano Martínez<h3>""<h3>")
