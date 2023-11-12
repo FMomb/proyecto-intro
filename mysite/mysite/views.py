05de0473462c9d390aee97acecccc860dc4c34e3
FMomb <fmomberg@usm.cl>
2023-11-12 02:32:04 -0300

    s

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -9,4 +9,10 @@ def index(request):
 #@login_required
 def Bdatos(request):
     voluntariado= clases.objects.all()
+    #voluntariado = clases.objects.filter(tematica= "Juventud" )
     return render(request, 'ver_voluntariado.html' , {"Clases": voluntariado })
+
+
+ #def buscartematica(request,tematica_elegida):
+  #  voluntariado= clases.objects.filter(tematica = tematica_elegida)
+   # return render(request, " ",  )
